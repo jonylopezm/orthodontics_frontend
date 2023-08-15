ab28210a924662fe3ad2fd7d15a680a0a3cfb319
Jony Lopez <jonslopez06@gmail.com>
2023-08-14 18:26:51 -0600

    patientDetails and modalNewAppointment

--- js/calendar.js
+++ js/calendar.js
@@ -9,10 +9,21 @@ document.addEventListener('DOMContentLoaded', function() {
         right: 'dayGridMonth,timeGridWeek,timeGridDay' // will normally be on the right. if RTL, will be on the left 
       }, 
        dateClick: function(info){
-        alert('Selecciono dia ' + info.dateStr);
+        openModal(info.dateStr);
       },
       locale:'es',
     });
     
     calendar.render();
   });
+
+  function openModal(date){
+    /* Abrir la ventana modal para crear nueva cita */
+    fetch('/components/modalNewAppointment.html')
+    .then(response => response.text())
+    .then(content => {
+        document.getElementById('modalContainer').innerHTML = content;
+        const modal = new bootstrap.Modal(document.getElementById('newAppointmentModal'));
+        modal.show();
+    });
+  }
